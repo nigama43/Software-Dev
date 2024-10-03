1fa754b67409072123b5930f2be8736f1b3885c5
nigama43 <88390007+nigama43@users.noreply.github.com>
2024-10-03 14:31:46 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,22 @@
 
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Create from "./Components/Create";
+import Read from "./Components/Read";
+import Update from "./Components/Update";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route exact path="/" element={<Create />} />
+          <Route exact path="/Read" element={<Read />} />
+          <Route path="/update" element={<Update />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
